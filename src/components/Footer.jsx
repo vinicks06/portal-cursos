26b8602ae7065e5c58e5f1d4e0411c57a7013225
VINICIUS CZECHOSKI COELHO <vinicius_c_coelho@estudante.sesisenai.org.br>
2025-05-28 17:16:24 -0300

    first commit

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white text-center p-4 mt-8">
-        <p>&copy; 2025 CursosOnline. Todos os direitos reservados.</p>
-      </footer>
-    );
-  }
-  
